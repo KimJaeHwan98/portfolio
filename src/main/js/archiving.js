027b90bf2f76a15cf8ea77069787a6500f3b4639
김재환 <jawhan98@naver.com>
2024-08-06 15:02:56 +0900

    완

--- src/main/js/archiving.js
+++ src/main/js/archiving.js
@@ -96,24 +96,24 @@ const Archiving = forwardRef((props, ref) => {
                         <li>학부 시절 수강한 수업들의 코딩 과제 소스 코드</li>
                     </ul>
                 </div>
-{/* 
+
                 <div className='archive-card'>
                     <div className="archive-img">
                         <img src={`${process.env.PUBLIC_URL}/img/archiving/tstory.png`} alt='tistory' />
                     </div>
                     <div className="archive-link">
-                        <a className='address' href='https://tistory.com' >tistory.com</a>
+                        <a className='address' href='https://kjh9809.tistory.com/' >https://kjh9809.tistory.com/</a>
                     </div>
                     <div className="archive-desc">
                         <label className='no-hover'>공부 및 지식 공유 목적의 블로그</label>입니다.
                     </div>
                     <ul className="archive-list">
                         <li>공부한 것을 진정한 나의 것으로 만들기 위한 기록</li>
-                        <li>개발자의 길을 걸으며 공부한 개발 관련 지식 정리</li>
-                        <li>학부 시절 배운 전공 수업들의 내용 복습 및 정리</li>
+                        {/* <li>개발자의 길을 걸으며 공부한 개발 관련 지식 정리</li> */}
+                        {/* <li>학부 시절 배운 전공 수업들의 내용 복습 및 정리</li> */}
                         <li>지식 공유를 통한 선한 영향력 행사</li>
                     </ul>
-                </div> */}
+                </div>
             </div>
         </div>
     )
